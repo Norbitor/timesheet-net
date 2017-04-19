4e9045c660978dd7763837b6d9cae2f72aa81d98
Norbert Langner <norbitor@gmail.com>
2017-04-19 22:23:51 +0200

    Add weekly report view

---
+++ timesheet-net/timesheet-net/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace timesheet_net.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
