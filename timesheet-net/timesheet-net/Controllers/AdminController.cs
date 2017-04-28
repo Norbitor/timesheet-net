3a49159ec20d77d294bdfd7fb48bdca78dd50504
Norbert Langner <norbitor@gmail.com>
2017-04-28 16:15:01 +0200

    Rename User List to Employees; fill view with data from database

--- timesheet-net/timesheet-net/Controllers/AdminController.cs
+++ timesheet-net/timesheet-net/Controllers/AdminController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using timesheet_net.Models;
 
 namespace timesheet_net.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult UserList()
+        public ActionResult Employees()
         {
             if (Session["EmployeeID"] != null)
             {
-                return View();
+                CheckUserPermission();
+                var entities = new TimesheetDBEntities();
+                return View(entities.Employees.ToList());
             }
             else
             {
@@ -25,6 +27,7 @@ namespace timesheet_net.Controllers
         {
             if (Session["EmployeeID"] != null)
             {
+                CheckUserPermission();
                 return View();
             }
             else
@@ -37,6 +40,7 @@ namespace timesheet_net.Controllers
         {
             if (Session["EmployeeID"] != null)
             {
+                CheckUserPermission();
                 return View();
             }
             else
@@ -49,11 +53,20 @@ namespace timesheet_net.Controllers
         {
             if (Session["EmployeeID"] != null)
             {
+                CheckUserPermission();
                 return View();
             }
             else
             {
                 return RedirectToAction("Login", "Account");
+            }
+        }
+
+        private void CheckUserPermission()
+        {
+            if ((int)Session["JobPosition"] != 1)
+            {
+                throw new UnauthorizedAccessException("Nie masz wystarczających uprawnień do oglądania tej witryny.");
             }
         }
     }
