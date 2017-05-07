32ec07b8a93188a9fe56465c0405fe0b3769cf0c
gradzka <monika.gradzka@tlen.pl>
2017-05-07 21:40:12 +0200

    Prototyp - timesheet - kontynuacja

--- timesheet-net/timesheet-net/Controllers/TimesheetController.cs
+++ timesheet-net/timesheet-net/Controllers/TimesheetController.cs
@@ -13,8 +13,9 @@ namespace timesheet_net.Controllers
     {
 
         // GET: Timesheet
-        public ActionResult Current()
+        public ActionResult Current(string projectName=null)
         {
+            List<SelectListItem> IDNameProject = new List<SelectListItem>();
             if (Session["EmployeeID"] != null)
             {
                 TimesheetDBEntities ctx = new TimesheetDBEntities();
@@ -23,28 +24,56 @@ namespace timesheet_net.Controllers
                 //list of project IDs where person is assigned
                 var listOfProjectIDs = ctx.ProjectMembers.Include(x=> x.Projects).Where(x => x.EmployeeID == loggedIn).OrderBy(x => x.ProjectMemberID).Select(x=>x.ProjectID).ToList();
                
-                if (listOfProjectIDs!=null) //if user is assigned to somewhere
+                if (listOfProjectIDs.Count()!=0) //if user is assigned to somewhere
                 {
-                    List<SelectListItem> IDNameProject = new List<SelectListItem>();
                     foreach(var item in listOfProjectIDs)
                     {
-                        var projectName = ctx.Projects.Where(x => x.ProjectID == item).Select(x => x.Name).FirstOrDefault();
-                        if (projectName != null)
+                        var projectNames = ctx.Projects.Where(x => x.ProjectID == item).Select(x => x.Name).FirstOrDefault();
+                        //if loggedIn is assigned to project...
+                        if (projectNames != null)
                         {
                             IDNameProject.Add(new SelectListItem
                             {
-                                Text = projectName.ToString(),
-                                Value= item.ToString()
+                                Text = projectNames.ToString(), //Name
+                                Value= item.ToString()          //ProjectID
                             });
                         }
                     }
+                    //if someone go to this section first time
+                    if (projectName==null)
+                    {
+                        //take first from list (ProjectID)
+                        int selectedprojectID = Int32.Parse(IDNameProject.First().Value);
+                        //take proper ProjectMemberID -> ProjectID && (int)Session["EmployeeID"]
+                        var projectMemberID = ctx.ProjectMembers.Where(x => x.ProjectID == selectedprojectID && x.EmployeeID == loggedIn).Select(x => x.ProjectMemberID).FirstOrDefault();
+                        //take proper Timesheet having ProjectMemberID
+                        if (projectMemberID > 0)
+                        {
+                            var timesheet = ctx.Timesheets.Where(x => x.ProjectMemberID == projectMemberID).Select(x => new { x.Start,x.Finish,x.TimesheetStateID }).FirstOrDefault();
+                            if (timesheet != null)
+                            {
+                                string timesheetStateName = ctx.TimesheetStates.Where(x => x.TimesheetStateID == timesheet.TimesheetStateID).Select(x => x.TimesheetStateName).FirstOrDefault();
+                                if (timesheetStateName!=null)
+                                {
+                                    ViewBag.projectName = IDNameProject.First().Text;
+                                    ViewBag.timesheetStart=timesheet.Start.ToString("yyyy-MM-dd");
+                                    ViewBag.timesheetFinish=timesheet.Finish.Date.ToString("yyyy-MM-dd");
+                                    ViewBag.timesheetStateName=timesheetStateName;
+                                }
+                            }
+
+                        }
+
+                    }
+                    else //TODO
+                    { }
 
                     return View(IDNameProject);
                 }
 
             }
 
-            return View();
+            return View(IDNameProject);
         }
 
         // GET: Timesheet
