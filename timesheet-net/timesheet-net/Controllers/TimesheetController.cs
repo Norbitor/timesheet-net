6ca1264c6132ff84a3357b9710ea2044ed8bd488
gradzka <monika.gradzka@tlen.pl>
2017-05-14 22:51:43 +0200

    Timesheet
    1. Przełączanie się między projektami
    2. Pobieranie zadań, wyświetlanie ich
    3. Częściowe podsumowanie przepracowanych godzin
    4. Wyświetlanie komentarza jako menu rozwijalnego
    5. Zmiana kontrolki input number na input+regex
    6. Poprawienie responsywności
    7. Poprawienie wyglądu
    8. Dodanie kontrolki tabeli

--- timesheet-net/timesheet-net/Controllers/TimesheetController.cs
+++ timesheet-net/timesheet-net/Controllers/TimesheetController.cs
@@ -13,7 +13,7 @@ namespace timesheet_net.Controllers
     {
 
         [HttpGet]
-        public ActionResult Current(string projectName=null)
+        public ActionResult Current()
         {
             List<SelectListItem> IDNameProject = new List<SelectListItem>();
             if (Session["EmployeeID"] != null)
@@ -23,50 +23,71 @@ namespace timesheet_net.Controllers
                 int loggedIn = (int)Session["EmployeeID"];
                 //list of project IDs where person is assigned
                 var listOfProjectIDs = ctx.ProjectMembers.Include(x => x.Projects).Where(x => x.EmployeeID == loggedIn).OrderBy(x => x.ProjectMemberID).Select(x => x.ProjectID).ToList();
-                if (listOfProjectIDs.Count()!=0) //if user is assigned to somewhere
+                if (listOfProjectIDs.Count() != 0) //if user is assigned to somewhere
                 {
-                    foreach(var item in listOfProjectIDs)
+                    foreach (var item in listOfProjectIDs)
                     {
                         var projectNames = ctx.Projects.Where(x => x.ProjectID == item).Select(x => x.Name).FirstOrDefault();
                         //if loggedIn is assigned to project...
                         if (projectNames != null)
                         {
-                            IDNameProject.Add(new SelectListItem
+                            if (Session["projectName"] != null)
                             {
-                                Text = projectNames.ToString(), //Name
-                                Value= item.ToString()          //ProjectID
-                            });
-                        }
-                    }
-                    //if someone go to this section first time
-                    if (projectName==null)
-                    {
-                        //take first from list (ProjectID)
-                        int selectedprojectID = Int32.Parse(IDNameProject.First().Value);
-                        //take proper ProjectMemberID -> ProjectID && (int)Session["EmployeeID"]
-                        var projectMemberID = ctx.ProjectMembers.Where(x => x.ProjectID == selectedprojectID && x.EmployeeID == loggedIn).Select(x => x.ProjectMemberID).FirstOrDefault();
-                        //take proper Timesheet having ProjectMemberID
-                        if (projectMemberID > 0)
-                        {
-                            var timesheet = ctx.Timesheets.Where(x => x.ProjectMemberID == projectMemberID).Select(x => new { x.Start,x.Finish,x.TimesheetStateID }).FirstOrDefault();
-                            if (timesheet != null)
-                            {
-                                string timesheetStateName = ctx.TimesheetStates.Where(x => x.TimesheetStateID == timesheet.TimesheetStateID).Select(x => x.TimesheetStateName).FirstOrDefault();
-                                if (timesheetStateName!=null)
+                                if (Session["projectName"].ToString() == projectNames.ToString())
                                 {
-                                    ViewBag.projectName = IDNameProject.First().Text;
-                                    ViewBag.timesheetStart=timesheet.Start.ToString("yyyy-MM-dd");
-                                    ViewBag.timesheetFinish=timesheet.Finish.Date.ToString("yyyy-MM-dd");
-                                    ViewBag.timesheetStateName=timesheetStateName;
+                                    IDNameProject.Insert(0, (new SelectListItem
+                                    {
+                                        Text = projectNames.ToString(),  //Name
+                                        Value = item.ToString()          //ProjectID
+                                    }));
+                                }
+                                else
+                                {
+                                    IDNameProject.Add(new SelectListItem
+                                    {
+                                        Text = projectNames.ToString(), //Name
+                                        Value = item.ToString()          //ProjectID
+                                    });
                                 }
                             }
-
+                            else
+                            {
+                                IDNameProject.Add(new SelectListItem
+                                {
+                                    Text = projectNames.ToString(), //Name
+                                    Value = item.ToString()          //ProjectID
+                                });
+                            }
                         }
-
                     }
-                    else //TODO
-                    { }
 
+                    //take first from list (ProjectID)
+                    int selectedprojectID = Int32.Parse(IDNameProject.First().Value);
+                    //take proper ProjectMemberID -> ProjectID && (int)Session["EmployeeID"]
+                    var projectMemberID = ctx.ProjectMembers.Where(x => x.ProjectID == selectedprojectID && x.EmployeeID == loggedIn).Select(x => x.ProjectMemberID).FirstOrDefault();
+                    //take proper Timesheet having ProjectMemberID
+                    if (projectMemberID > 0)
+                    {
+                        //info about the projecy
+                        var timesheet = ctx.Timesheets.Where(x => x.ProjectMemberID == projectMemberID).Select(x => new {x.TimesheetID, x.Start, x.Finish, x.TimesheetStateID }).FirstOrDefault();
+                        if (timesheet != null)
+                        {
+                            string timesheetStateName = ctx.TimesheetStates.Where(x => x.TimesheetStateID == timesheet.TimesheetStateID).Select(x => x.TimesheetStateName).FirstOrDefault();
+                            if (timesheetStateName != null)
+                            {
+                                ViewBag.projectName = IDNameProject.First().Text;
+                                ViewBag.timesheetStart = timesheet.Start.ToString("yyyy-MM-dd");
+                                ViewBag.timesheetFinish = timesheet.Finish.Date.ToString("yyyy-MM-dd");
+                                ViewBag.timesheetStateName = timesheetStateName;
+                            }
+                            //list of tasks
+                            var tasks = ctx.Tasks.Where(x => x.TimesheetID == timesheet.TimesheetID).ToList(); ;
+                            if (tasks.Count()!=0)
+                            {
+                                Session["tasks"] = tasks;
+                            }
+                        }
+                    }
                     return View(IDNameProject);
                 }
 
@@ -79,7 +100,7 @@ namespace timesheet_net.Controllers
         {
             if (Session["EmployeeID"] != null)
             {
-                //session
+
                 return RedirectToAction("Current", "Timesheet");
             }
             else
@@ -89,13 +110,18 @@ namespace timesheet_net.Controllers
         }
 
         [HttpPost]
-        public ActionResult ChangeTimesheet(string projectName)
+        public ActionResult ChangeTimesheet(string projectID)
         {
-            //projectName is the identyfier of the project!
-            //string -> int
             if (Session["EmployeeID"] != null)
             {
                 //session
+                TimesheetDBEntities ctx = new TimesheetDBEntities();
+                int projectIdent = Int32.Parse(projectID);
+                var projectNames = ctx.Projects.Where(x => x.ProjectID == projectIdent).Select(x => x.Name).FirstOrDefault();
+                if (projectNames != null)
+                {
+                    Session["projectName"] = projectNames;
+                }
                 return RedirectToAction("Current", "Timesheet");
             }
             else
